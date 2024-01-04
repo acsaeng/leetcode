32af1a3cf02116427dc630c8b728bd1e4c2a3c61
Aron Saengchan <acsaeng@gmail.com>
2024-01-03 20:10:52 -0700

    Optimized solution

--- 121-best_time_to_buy_and_sell_stock.py
+++ 121-best_time_to_buy_and_sell_stock.py
@@ -6,8 +6,8 @@ class Solution:
     for price in prices[1:]:
       if price < min_price:
         min_price = price
-      elif price - min_price > max_profit:
-        max_profit = price - min_price 
+      else:
+        max_profit = max(max_profit, price - min_price)
 
     return max_profit
 
