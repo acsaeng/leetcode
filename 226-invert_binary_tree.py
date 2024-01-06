8be70f149aa656824285cd759f817c778df8dc47
Aron Saengchan <acsaeng@gmail.com>
2024-01-05 18:17:01 -0800

    Modified solution to DFS

--- 226-invert_binary_tree.py
+++ 226-invert_binary_tree.py
@@ -5,22 +5,11 @@ class Solution:
   def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
     if not root:
       return
+    else:
+      root.left, root.right = root.right, root.left
+      self.invertTree(root.left)
+      self.invertTree(root.right)
     
-    queue = [root]
-
-    while queue:
-      node = queue.pop(0)
-
-      temp = node.left
-      node.left = node.right
-      node.right = temp
-
-      if node.left:
-        queue.append(node.left)
-
-      if node.right:
-        queue.append(node.right)
-
     return root
 
 
