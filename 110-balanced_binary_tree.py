21aab1a75bc2d637e486d445528a7c579b076167
Aron Saengchan <acsaeng@gmail.com>
2024-01-09 21:12:40 -0700

    Simplified logic

--- 110-balanced_binary_tree.py
+++ 110-balanced_binary_tree.py
@@ -6,20 +6,14 @@ class Solution:
     return self.check_balanced_tree(root)[0]
   
   def check_balanced_tree(self, root):
-    if not root: 
+    if not root:
       return [True, 0]
-    elif not root.left and not root.right:
-      return [True, 1]
-    elif not root.left:
-      right = self.check_balanced_tree(root.right)
-      return [right[0] and right[1] <= 1, right[1] + 1]
-    elif not root.right:
-      left = self.check_balanced_tree(root.left)
-      return [left[0] and left[1] <= 1, left[1] + 1]
-    else:
-      left = self.check_balanced_tree(root.left)
-      right = self.check_balanced_tree(root.right)
-      return [left[0] and right[0] and abs(left[1] - right[1]) <= 1, max(left[1], right[1]) + 1]
+
+    left = self.check_balanced_tree(root.left)
+    right = self.check_balanced_tree(root.right)
+    is_balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+
+    return [is_balanced, max(left[1], right[1]) + 1]
   
 
 print('Case 1: root = [3, 9, 20, null, null, 15, 7]')
@@ -30,6 +24,3 @@ print('Answer:', Solution().isBalanced(list_to_binary_tree([1, 2, 2, 3, 3, None,
 
 print('\nCase 3: root = []')
 print('Answer:', Solution().isBalanced(list_to_binary_tree([])))
-
-print('\nCase: root = [1, 2, 2, 3, null, null, 3, 4, null, null, 4]')
-print('Answer:', Solution().isBalanced(list_to_binary_tree([1, 2, 2, 3, None, None, 3, 4, None, None, 4])))
