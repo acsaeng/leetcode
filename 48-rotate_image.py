43f10604fa5827dfe93b54196e5ce52cd058aca7
Aron Saengchan <acsaeng@gmail.com>
2024-01-10 20:23:55 -0700

    Removed unused import

--- 48-rotate_image.py
+++ 48-rotate_image.py
@@ -1,5 +1,3 @@
-import itertools
-
 class Solution:
   def rotate(self, matrix: list[list[int]]) -> None:
     """
@@ -17,14 +15,12 @@ class Solution:
         matrix[i][j], matrix[i][len(matrix[i]) - 1 - j] = matrix[i][len(matrix[i]) - 1 - j], matrix[i][j]
 
 
-solution = Solution()
-
-matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-solution.rotate(matrix)
 print('Case 1: matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]')
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+Solution().rotate(matrix)
 print('Answer:', matrix)
 
-matrix = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
-solution.rotate(matrix)
 print('\nCase 2: matrix = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]')
+matrix = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
+Solution().rotate(matrix)
 print('Answer:', matrix)
