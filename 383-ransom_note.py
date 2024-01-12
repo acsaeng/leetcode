b18bdd7d76eb93b5c3e61690f049062dded36646
Aron Saengchan <acsaeng@gmail.com>
2024-01-11 20:21:10 -0700

    Created solution

---
+++ 383-ransom_note.py
@@ -0,0 +1,24 @@
+class Solution:
+  def canConstruct(self, ransomNote: str, magazine: str) -> bool: 
+    magazine_map = {}
+
+    for char in magazine:
+      magazine_map[char] = magazine_map.get(char, 0) + 1
+
+    for char in ransomNote:
+      if char in magazine_map and magazine_map[char] > 0:
+        magazine_map[char] -= 1
+      else:
+        return False
+    
+    return True
+
+
+print('Case 1: ransomNote = "a", magazine = "b"')
+print('Answer:', Solution().canConstruct('a', 'b'))
+
+print('\nCase 2: ransomNote = "aa", magazine = "ab"')
+print('Answer:', Solution().canConstruct('aa', 'ab'))
+
+print('\nCase 3: ransomNote = "aa", magazine = "aab"')
+print('Answer:', Solution().canConstruct('aa', 'aab'))    
