7124b5694ee1c0acfb6a7f6620f2b9a48c9b9d2b
Aron Saengchan <acsaeng@gmail.com>
2024-01-12 23:00:30 -0700

    Implemented standard solution

--- 191-number_of_1_bits.py
+++ 191-number_of_1_bits.py
@@ -1,16 +1,19 @@
 class Solution:
-    def hammingWeight(self, n: int) -> int:
-        n_binary = bin(n)
-        return str(n_binary).count('1')
+  def hammingWeight(self, n: int) -> int:
+    num_1s = 0
 
-
-solution = Solution()
+    while n:
+      n &= (n - 1)
+      num_1s += 1
+    
+    return num_1s
+  
 
 print('Case 1: n = 00000000000000000000000000001011')
-print('Answer:', solution.hammingWeight(11))
+print('Answer:', Solution().hammingWeight(11))
 
 print('\nCase 2: n = 00000000000000000000000010000000')
-print('Answer:', solution.hammingWeight(128))
+print('Answer:', Solution().hammingWeight(128))
 
 print('\nCase 3: n = 11111111111111111111111111111101')
-print('Answer:', solution.hammingWeight(4294967293))
+print('Answer:', Solution().hammingWeight(4294967293))
