9ecabc2161f47cb5dd4742f5c52c4b6c3ec8f473
Aron Saengchan <acsaeng@gmail.com>
2024-01-13 23:32:59 -0700

    Optimized solution

--- 198-house_robber.py
+++ 198-house_robber.py
@@ -3,21 +3,18 @@ class Solution(object):
     if len(nums) == 1:
       return nums[0]
 
-    last_two_houses = [nums[0], nums[1] if nums[1] > nums[0] else nums[0]]  # Most money collected from last two houses
-    max_money = max(last_two_houses)
+    last_two_houses = [0, 0]
 
-    for i in range(2, len(nums)):
+    for i in range(len(nums)):
       max_money = max(nums[i] + last_two_houses[0], last_two_houses[1])
       last_two_houses[0] = last_two_houses[1]
       last_two_houses[1] = max_money
 
     return max_money
-  
 
-solution = Solution()
 
 print('Case 1: nums = [1, 2, 3, 1]')
-print('Answer:', solution.rob([1, 2, 3, 1]))
+print('Answer:', Solution().rob([1, 2, 3, 1]))
 
 print('\nCase 2: nums = [2, 7, 9, 3, 1]')
-print('Answer:', solution.rob([2, 7, 9, 3, 1]))
+print('Answer:', Solution().rob([2, 7, 9, 3, 1]))
