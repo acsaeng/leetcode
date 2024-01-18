d96a36cbca35db8cee38f782c8f4f8c411044f15
Aron Saengchan <acsaeng@gmail.com>
2024-01-17 21:23:58 -0700

    Created solution

---
+++ 53-maximum-subarray.py
@@ -0,0 +1,25 @@
+class Solution:
+  def maxSubArray(self, nums: list[int]) -> int:
+    local_max_sum = 0
+    global_max_sum = nums[0]
+
+    for num in nums:
+      local_max_sum += num
+    
+      if local_max_sum > global_max_sum:
+        global_max_sum = local_max_sum
+
+      if local_max_sum < 0:
+        local_max_sum = 0
+    
+    return global_max_sum
+
+
+print('Case 1: nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]')
+print('Answer:', Solution().maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
+
+print('\nCase 2: nums = [1]')
+print('Answer:', Solution().maxSubArray([1]))
+
+print('\nCase 3: nums = [5, 4, -1, 7, 8]')
+print('Answer:', Solution().maxSubArray([5, 4, -1, 7, 8]))
