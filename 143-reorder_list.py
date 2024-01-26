e95dfe60f654dfd6f108d0cf7378b2aee4cdc488
Aron Saengchan <acsaeng@gmail.com>
2024-01-25 23:03:44 -0700

    Implemented merge technique

--- 143-reorder_list.py
+++ 143-reorder_list.py
@@ -3,20 +3,33 @@ from utils.linked_list import ListNode, linked_list_to_list, list_to_linked_list
 
 class Solution:
   def reorderList(self, head: Optional[ListNode]) -> None:
-    tail = head
+    slow = head
+    fast = head.next
 
-    while tail.next:
-      tail = tail.next
-    
-    while head != tail:
-      head.next, tail.next = tail, head.next
-      head = tail.next
+    # Find middle of linked list
+    while fast and fast.next:
+      slow = slow.next
+      fast = fast.next.next
 
-      while tail.next.next != head:
-        tail = tail.next
+    # Reverse pointers in second half of linked list
+    cursor = slow.next
+    prev_cursor = slow.next = None
     
-    head.next = None
+    while cursor:
+      next_cursor = cursor.next
+      cursor.next = prev_cursor
+      prev_cursor = cursor
+      cursor = next_cursor
     
+    # Merge the two halved linked lists
+    tail = prev_cursor
+
+    while tail:
+      next_head, next_tail = head.next, tail.next
+      head.next = tail
+      tail.next = next_head
+      head, tail = next_head, next_tail
+
 
 print('Case 1: head = [1, 2, 3, 4]')
 head = list_to_linked_list([1, 2, 3, 4])
