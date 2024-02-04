1dda165b0671d8d2194d622d9bbde2769a28ee46
Aron Saengchan <acsaeng@gmail.com>
2024-02-04 14:22:20 -0700

    Created solution

---
+++ 153-find_minimum_in_rotated_sorted_array.py
@@ -0,0 +1,23 @@
+class Solution:
+  def findMin(self, nums: list[int]) -> int:
+    left, right = 0, len(nums) - 1
+
+    while left < right:
+      middle = left + ((right - left) // 2)
+
+      if nums[middle] > nums[right]:
+        left = middle + 1
+      else:
+        right = middle
+    
+    return nums[left]
+
+
+print('Case 1: nums = [3, 4, 5, 1, 2]')
+print('Answer:', Solution().findMin([3, 4, 5, 1, 2]))
+
+print('\nCase 2: nums = [4, 5, 6, 7, 0, 1, 2]')
+print('Answer:', Solution().findMin([4, 5, 6, 7, 0, 1, 2]))
+
+print('\nCase 2: nums = [11, 13, 15, 17]')
+print('Answer:', Solution().findMin([11, 13, 15, 17]))
