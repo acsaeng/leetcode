43bc0ce8324036a2369d74da44c436aa9b4d1e5e
Aron Saengchan <acsaeng@gmail.com>
2024-02-04 20:56:07 -0700

    Created solution

---
+++ 100-same_tree.py
@@ -0,0 +1,21 @@
+from typing import Optional
+from utils.binary_tree import list_to_binary_tree, TreeNode
+
+class Solution:
+  def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+    if not p and not q:
+      return True
+    elif p and q and p.val == q.val:
+      return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+    else:
+      return False
+
+
+print('Case 1: p = [1, 2, 3], q = [1, 2, 3]')
+print('Answer:', Solution().isSameTree(list_to_binary_tree([1, 2, 3]), list_to_binary_tree([1, 2, 3])))
+                                       
+print('\nCase 2: p = [1, 2], q = [1, null, 2]')
+print('Answer:', Solution().isSameTree(list_to_binary_tree([1, 2]), list_to_binary_tree([1, None, 2])))
+
+print('\nCase 3: p = [1, 2, 1], q = [1, 1, 2]')
+print('Answer:', Solution().isSameTree(list_to_binary_tree([1, 2, 1]), list_to_binary_tree([1, 1, 2])))
