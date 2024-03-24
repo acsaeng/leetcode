5d8a5e092e9ed39f782b20e89aa6968ba5451a58
Aron Saengchan <acsaeng@gmail.com>
2024-03-23 21:19:41 -0600

    Implemented length delimiter

--- 271-encode_and_decode_strings.py
+++ 271-encode_and_decode_strings.py
@@ -1,6 +1,35 @@
 class Solution:
   def encode(self, strs: list[str]) -> str:
-    return ':;'.join(strs) if strs else None
+    encoded_str = ''
+
+    for s in strs:
+      encoded_str += str(len(s)) + '#' + s
+
+    return encoded_str
 
   def decode(self, s: str) -> list[str]:
-    return s.split(':;') if s is not None else []
+    decoded_str = []
+    start_index = 0
+
+    while start_index < len(s):
+      end_index = start_index
+      
+      while s[end_index] != '#':
+        end_index += 1
+
+      str_len = int(s[start_index:end_index])
+
+      start_index = end_index + 1
+      end_index = start_index + str_len
+      decoded_str.append(s[start_index:end_index])
+
+      start_index = end_index
+    
+    return decoded_str
+
+
+print('Case 1: ["leet", "code", "love", "you"]')
+print('Answer:', Solution().decode(Solution().encode(['leet', 'code', 'love', 'you'])))
+
+print('\nCase 2: ["we", "say", ":", "yes"]')
+print('Answer:', Solution().decode(Solution().encode(['we', 'say', ':', 'yes'])))
