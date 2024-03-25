445e3a5dcaf1df0bf0c259c85ceeacbf9006b46b
Aron Saengchan <acsaeng@gmail.com>
2024-03-24 22:54:03 -0600

    Removed global variable

--- 1448-count_good_nodes_in_binary_tree.py
+++ 1448-count_good_nodes_in_binary_tree.py
@@ -1,22 +1,16 @@
 from utils.binary_tree import TreeNode, list_to_binary_tree
 
 class Solution:
-  def __init__(self):
-    self.num_good_nodes = 0
-
   def goodNodes(self, root: TreeNode) -> int:
-    self.dfs(root, float('-inf'))
-    return self.num_good_nodes
+    return self.dfs(root, root.val)
   
   def dfs(self, node, max_val):
     if not node:
-      return
+      return 0
     else:
-      if node.val >= max_val:
-        self.num_good_nodes += 1
-      
-      self.dfs(node.left, max(node.val, max_val))
-      self.dfs(node.right, max(node.val, max_val))
+      increment = 1 if node.val >= max_val else 0
+      max_val = max(node.val, max_val)
+      return self.dfs(node.left, max_val) + self.dfs(node.right, max_val) + increment
 
 
 print('Case 1: root = [3, 1, 4, 3, null, 1, 5]')
