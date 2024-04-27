3846b52b0fedb9cc6aacb4dae0b54bd3101a24b3
Aron Saengchan <acsaeng@gmail.com>
2024-04-27 17:15:42 -0600

    Simplified methods

--- 146-lru_cache.py
+++ 146-lru_cache.py
@@ -1,4 +1,4 @@
-class Node:
+class DoubleListNode:
   def __init__(self, key=0, val=0, prev=None, next=None):
     self.key = key
     self.val = val
@@ -8,41 +8,44 @@ class Node:
 
 class LRUCache:
   def __init__(self, capacity: int):
+    self.cache = {}  # { key: int, value: DoubleListNode }
     self.capacity = capacity
-    self.cache = {}
-
-    # left is least recently used, right is most recently used
-    self.left, self.right = Node(), Node()
-    self.left.next, self.right.prev = self.right, self.left
-
+    
+    # left is least recently used (LRU), right is most recently used (MRU)
+    self.lru_node, self.mru_node = DoubleListNode(), DoubleListNode()
+    self.lru_node.next, self.mru_node.prev = self.mru_node, self.lru_node
+    
   def get(self, key: int) -> int:
     if key in self.cache:
-      self.remove(self.cache[key])
-      self.insert(self.cache[key])
-      return self.cache[key].val
+      value = self.cache[key].val
+      self.delete_entry(key)
+      self.insert_entry(key, value)
+      return value
     else:
       return -1
-
+        
   def put(self, key: int, value: int) -> None:
     if key in self.cache:
-      self.remove(self.cache[key])
+      self.delete_entry(key)
+    elif len(self.cache) == self.capacity:
+      self.delete_entry(self.lru_node.next.key)
+    
+    self.insert_entry(key, value)
+    
+  def insert_entry(self, key, value):
+    new_node = DoubleListNode(key, value)
+    self.cache[key] = new_node
 
-    self.cache[key] = Node(key, value)
-    self.insert(self.cache[key])
+    prev, next = self.mru_node.prev, self.mru_node
+    new_node.prev, new_node.next = prev, next
+    prev.next, next.prev = new_node, new_node
 
-    if len(self.cache) > self.capacity:
-      lru = self.left.next
-      self.remove(lru)
-      del self.cache[lru.key]
-  
-  def insert(self, node):
-    prev, next = self.right.prev, self.right
-    prev.next = next.prev = node
-    node.prev, node.next = prev, next
-
-  def remove(self, node):
+  def delete_entry(self, key):
+    node = self.cache[key]
     prev, next = node.prev, node.next
     prev.next, next.prev = next, prev
+
+    del self.cache[key]
 
 
 print('Case 1: ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"], [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]')
